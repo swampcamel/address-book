7e07ee64a1ae850898527765f6f32c3cdc216b15
Brendan Shea and William Wittenbrock <brendan+william@>
2018-08-20 10:07:20 -0700

    Add phone number field.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 // BACKEND LOGIC
 
-function Contact(first, last) {
+function Contact(first, last, email, phone) {
   this.firstName = (first);
   this.lastName = (last);
+  this.email = (email);
+  this.phone = (phone);
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
 }
 
 
@@ -20,10 +26,12 @@ $(document).ready(function(){
 
     var inputtedFirstName = $("input#first-name").val();
     var inputtedLastName = $("input#last-name").val();
+    var inputtedEmail = $("input#email").val();
+    var inputtedPhone = $("input#phone").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEmail, inputtedPhone);
 
-    $("ul#contacts").append("<li><span class='contacts'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contacts'>" + newContact.fullName() + "</span></li>");
 
     $("input#first-name").val("");
     $("input#last-name").val("");
@@ -33,6 +41,8 @@ $(document).ready(function(){
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $(".email").text(newContact.email);
+      $(".phone").text(newContact.phone);
     });
 
   });
