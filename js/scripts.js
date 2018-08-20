99a02a9fcbad34f4002d8010091fffc3c383f51e
Brendan Shea and William Wittenbrock <brendan@>
2018-08-20 09:44:21 -0700

    Make contacts list clickable and show stored information.

--- js/scripts.js
+++ js/scripts.js
@@ -28,5 +28,12 @@ $(document).ready(function(){
     $("input#first-name").val("");
     $("input#last-name").val("");
 
+    $(".contacts").last().click(function(){
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
   });
 });
