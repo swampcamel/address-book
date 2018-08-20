9a189897d5dcac916e321a9586da2af5a4f0d1e1
Brendan Shea and William Wittenbrock <brendan+william@>
2018-08-20 09:26:31 -0700

    Add basic HTML for address book and JavaScript to pull inputs.

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// BACKEND LOGIC
+
+
+
+function Contact(first, last) {
+  this.firstName = (first);
+  this.lastName = (last);
+}
+
+
+
+
+
+
+
+
+
+// FRONT END LOGIC
+$(document).ready(function(){
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#first-name").val();
+    var inputtedLastName = $("input#last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contacts'>" + newContact.firstName + "</span></li>");
+
+    $("input#first-name").val("");
+    $("input#last-name").val("");
+
+  });
+});
